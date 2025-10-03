3da030967ae32ad1f26bec24e65c4be962e69c66
Ramesh Kanuri <74103407+Ramesh-debug@users.noreply.github.com>
2025-10-03 18:34:01 +0530

    first commit

---
+++ src/test/java/com/project/coding_exercise/CodingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.coding_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
